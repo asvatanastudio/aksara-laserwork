75715ee08c84fba5af4e9081e84c845eb8a4a258
asvatanastudio <asvatanaplayground@gmail.com>
2025-10-16 14:36:25 +0700

    menghapus app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
-// 1. Impor komponen FigmaHome Anda
 import FigmaHome from "./components/FigmaHome.js";
+//import "./App.css";
 
-// (Jika Anda menggunakan CSS, biarkan impor CSS jika ada)
-import "./App.css"; // atau nama file CSS lainnya
-
-// Ini adalah komponen utama aplikasi Anda
 function App() {
-  // 2. Tampilkan komponen FigmaHome di sini
   return <FigmaHome />;
 }
 
